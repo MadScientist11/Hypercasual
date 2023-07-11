fca8e48a127ea78818ebf7e4af649d75e8017940
ashirogimuta <quizandpuzzle@gmail.com>
2023-07-11 11:59:16 +0300

    Add basic conveyor, item spawn

---
+++ Assets/Scripts/Conveyor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Hypercasual
+{
+    public class Conveyor : MonoBehaviour
+    {
+        public Item _itemPrefab;
+        public Transform _spawnPoint;
+        private void Start()
+        {
+            StartCoroutine(StartAssemblyLine());
+        }
+
+
+        private IEnumerator StartAssemblyLine()
+        {
+            float yExtent = _itemPrefab.GetComponent<Collider>().bounds.extents.y;
+
+            while (true)
+            {
+                Item item = Instantiate(_itemPrefab, _spawnPoint.position + new Vector3(0,yExtent,0), Quaternion.identity);
+                yield return new WaitForSeconds(8);
+            }
+        }
+        
+    }
+}
