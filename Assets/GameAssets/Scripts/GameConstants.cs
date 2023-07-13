0844313b7b9d2ee0644b58962cb022f77b6f2487
ashirogimuta <quizandpuzzle@gmail.com>
2023-07-13 13:48:43 +0300

    Move Game Assets to separate folder

---
+++ Assets/GameAssets/Scripts/GameConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace Hypercasual
+{
+    public class GameConstants
+    {
+        public class Scenes
+        {
+            public const string BootPath = "Assets/GameAssets/Scenes/Boot.unity";
+            public const string GamePath = "Assets/GameAssets/Scenes/Game.unity";
+        }
+
+    }
+}
